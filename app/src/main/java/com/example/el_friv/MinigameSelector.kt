a5209f069c671e15320022d6fd3bb67d2f8536e3
Morgan <jan.moran.r@gmail.com>
2022-04-30 01:30:29 +0200

    PrimeraVersio-dissabte-1:30

---
+++ app/src/main/java/com/example/el_friv/MinigameSelector.kt
@@ -0,0 +1,21 @@
+package com.example.el_friv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+class MinigameSelector : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_minigame_selector)
+
+        val btnTresEnRalla = findViewById<Button>(R.id.btnTresEnRaya)
+
+
+        btnTresEnRalla.setOnClickListener {
+            val intent = Intent(this@MinigameSelector, TresEnRalla::class.java)
+            startActivity(intent)
+        }
+    }
+}
