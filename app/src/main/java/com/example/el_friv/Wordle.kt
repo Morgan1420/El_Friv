2a73b6d2f8b6ab974fe5ba92d64ebef80cc4d122
GeriiGarcia <gerardgarciagros@gmail.com>
2022-04-30 13:58:14 +0200

    Wordle

---
+++ app/src/main/java/com/example/el_friv/Wordle.kt
@@ -0,0 +1,11 @@
+package com.example.el_friv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Wordle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wordle)
+    }
+}
