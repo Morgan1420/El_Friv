64896eb8026e3ae971d9a720a88c59e3f52e4ae8
Morgan <jan.moran.r@gmail.com>
2022-04-30 13:59:04 +0200

    FallingBricks

---
+++ app/src/main/java/com/example/el_friv/FallingBricks.kt
@@ -0,0 +1,11 @@
+package com.example.el_friv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FallingBricks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_falling_bricks)
+    }
+}
